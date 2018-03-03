4190194e5d65754d685140dc953ee8e6f95d11ee
sachinika <37016365+sachinika@users.noreply.github.com>
2018-03-03 08:00:35 -0800

    Create spaces.c

---
+++ spaces.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i,spaces;
+char str[50];
+printf("enter th string");
+scanf("%s",a);
+for(i=0;a[0]!="\0";i++)
+{
+if(str[i]=' ')
+{
+spaces++;
+}
+}
+printf("number of spaces is %d",spaces);
+}
