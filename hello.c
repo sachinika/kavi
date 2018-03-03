917213073141bd2ba54c1c96fe2dc0d87355af3d
sachinika <37016365+sachinika@users.noreply.github.com>
2018-03-03 17:59:09 +0530

    hello.c

---
+++ hello.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[50];
+    int length;
+    int i;
+    int count;
+    printf("Enter the string\n");
+    gets(str);
+    length=strlen(str);
+        if(str[i]==' ')
+        {
+            count++;
+  printf("%d",count+1);
+  }
+  return 0;
+}
