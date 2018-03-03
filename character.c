20e5597d463d2cdd6224108e70e9268cfef30eb8
sachinika <37016365+sachinika@users.noreply.github.com>
2018-03-03 08:44:29 -0800

    Create character.c

---
+++ character.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int i,count char=0;
+char str[50];
+printf("enter th string");
+scanf("%s",str);
+for(i=0;a[0]!="\0";i++)
+{
+if(str[i]=' ')
+{
+count=char++;
+}
+}
+printf("number of char is %d",char);
+return 0;
+}
