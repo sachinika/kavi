c5688f3b7778324e95a07c636c8726fc2712898b
sachinika <37016365+sachinika@users.noreply.github.com>
2018-03-03 08:21:14 -0800

    Create lines.c

---
+++ lines.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i,lines=0;
+char str[50];
+printf("enter th string");
+scanf("%s",str);
+for(i=0;a[0]!="\0";i++)
+{
+if(str[i]=' ')
+{
+lines=lines+1;
+}
+}
+printf("number of lines is %d",lines);
+}
