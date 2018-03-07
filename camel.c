8477264ac1db705df14ef7daf72de004c04818bf
sachinika <37016365+sachinika@users.noreply.github.com>
2018-03-07 17:25:03 +0530

    Create camel.c

---
+++ camel.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int casestr( char* str)
+{ 
+int i;
+char ch;
+ch=str[i];
+for(i=0;i=ch;i++){
+if(islower(ch))
+{ ch=toupper(ch);
+  printf("\n ch")
+  } else{ printf("\n changed");}}return 0;
+  }
+  int main()
+  { char* str;
+   printf("\n enter the string");
+   scanf("%s",&str);
+   casestr(str);
+   return 0;
+}
+
