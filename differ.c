ea2493c7f518758ff60a602f7a9690a4db716bbc
sachinika <37016365+sachinika@users.noreply.github.com>
2018-03-07 17:41:02 +0530

    Create differ.c

---
+++ differ.c
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() 
+{ string s1,s2;
+  cout<<"enter strings";
+  cin>>s1>>s2;
+  int s=s1.length(),r=s2.length();
+  if(s==r)
+  { cout<<"strings are differ";
+   {
+     cout<<"yes";
+   }
+  } else{ cout<<"strings are not differ";
+         {
+           cout<<"no";
+        }
+        }
+  return 0;
+  }
