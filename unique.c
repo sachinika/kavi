6a7bc0c9afd8aa63fbea940c68145c761cd26188
sachinika <37016365+sachinika@users.noreply.github.com>
2018-03-08 17:13:45 +0530

    Update unique.c

--- unique.c
+++ unique.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 int main() 
 {
-    int l;
+    int lh;
     scanf("%d",&l);
-  int a[l],i,j=0;
-  for(i=0;i<l;i++)
+  int a[lh],i,j=0;
+  for(i=0;i<lh;i++)
   scanf("%d",&a[i]);
-    for(i=0;i<l;i++)
+    for(i=0;i<lh;i++)
     {
       if(a[j]==a[i+1])
          {
            printf("\n%d",a[j]);
-           for(j=i+1;j<l;j++)
+           for(j=i+1;j<lh;j++)
            {
            a[j]=a[j+1];
            a[l]='\0';
-           l--;
+           lh--;
                   }
          j++;
          }
