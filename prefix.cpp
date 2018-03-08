c40298ce35a0e2b996967a29bc8c47f7c17bce19
sachinika <37016365+sachinika@users.noreply.github.com>
2018-03-08 18:00:07 +0530

    Create prefix.cpp

---
+++ prefix.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() 
+{ string s1,s2;
+  cout<<"enter strings";
+  cin>>s1>>s2;
+  int s=s1.length(),r=s2.length();
+  if(s==r)
+  { cout<<"strings are equal";
+   {
+     cout<<"\n string";
+   }
+  } else{ cout<<"strings are not equal";
+         {
+           cout<<"\n the string is not equal in prefix";
+        }
+        }
+  return 0;
+  
