cb63f787e469f2c91f8d0f37a3c603b4582a8ade
sachinika <37016365+sachinika@users.noreply.github.com>
2018-03-13 17:29:50 +0530

    Update and rename prime.c to primes.c

--- primes.c
+++ primes.c
@@ -4,8 +4,7 @@
 void main()
 {
     int num1, num2, i, j, flag, temp, count = 0;
- 
-    printf("Enter the value of num1 and num2 \n");
+ printf("Enter the value of num1 and num2 \n");
     scanf("%d %d", &num1, &num2);
     if (num2 < 2)
     {
