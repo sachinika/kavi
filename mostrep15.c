bd0d7dbe4b6c9e32f2dc046bdfb58c219ed0b432
sachinika <37016365+sachinika@users.noreply.github.com>
2018-05-21 11:16:36 +0530

    Create mostrep15.c

---
+++ mostrep15.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+int main(void) {
+	 char ch[10],k;
+    int i,j,max=0,c=0;
+    printf("Enter a String : ");
+    scanf("%s",ch);
+        for(i=0;ch[i]!='\0';i++)
+    {
+        for(j=i+1;ch[j]!='\0';j++)
+        {
+            if(ch[i]==ch[j])
+            {
+                c=c+1;
+            }
+            if(c>max)
+            {
+                k=ch[i];
+                max=c;
+            }
+        }
+ 
+    }
+    printf("%c",k);
+	return 0;
+}
