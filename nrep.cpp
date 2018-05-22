918d54f24e73ae5fd4d211d6f7bb5b39ee4e17c3
sachinika <37016365+sachinika@users.noreply.github.com>
2018-05-22 10:34:28 +0530

    Create nrep.cpp

---
+++ nrep.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int firstNonRepeating(int arr[], int n)
+{
+	for (int i = 0; i < n; i++) {
+		int j;
+		for (j=0; j<n; j++)
+			if (i != j && arr[i] == arr[j])
+				break;
+		if (j == n)
+			return arr[i];
+	}
+	return -1;
+}
+int main()
+{
+	int arr[] = { 9, 4, 9, 9, 4, 3};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	cout << firstNonRepeating(arr,n);
+	return 0;
+}
