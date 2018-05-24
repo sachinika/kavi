08422a3ef287a6ac3769b2dcf78849e56c4e7d94
sachinika <37016365+sachinika@users.noreply.github.com>
2018-05-24 16:32:31 +0530

    Create numcount.cpp

---
+++ numcount.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+void main()
+{
+int no,a=0;
+cout<<"Enter any num : ";
+cin>>no;
+while(no>0)
+{
+no=no/10;
+a++;
+}
+cout<<"\n no. of digits in given number is: "<<a;
+getch();
+}
